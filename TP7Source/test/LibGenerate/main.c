d8ea4b0d0e6f29d80edfba6bd376ea9b66c15432
ELBEZ samuel <elbez@informatique.univ-paris-diderot.fr>
2017-11-27 17:27:42 +0100

    main pour libGenerate

--- TP7Source/test/LibGenerate/main.c
+++ TP7Source/test/LibGenerate/main.c
@@ -15,8 +15,13 @@
 */
 
 #include "../../src/generate.h"
+#include "../../src/relation.h"
 
 int main(int argc, char const *argv[]) {
 
+	short tab[12] = {0,1,2,3,4,5,6,7,8,9,10,11};
+	createDirectory("test");
+	generate("test", tab, 12);
+	printf("On attend 2 : on a %d\n", countFileInDirectory("test"));
 	return EXIT_SUCCESS;
 }
