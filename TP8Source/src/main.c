e18923b792ff09d3a58ed9a268a412e4e19d81b3
PierrickJACQUETTE <pierrick.jacquette@gmail.com>
2017-12-04 11:44:48 +0100

    change makefile and add main.c

---
+++ TP8Source/src/main.c
@@ -0,0 +1,24 @@
+/**
+* TP n°: 8
+*
+* Titre du TP : Bulk B Tree
+*
+* Date : 01/12/2017
+*
+* Nom : BESSA & ELBEZ & JACQUETTE
+* Prenom : Alexandre & Samuel & Pierrick
+*
+* email :
+alexandrebessa26@gmail.com
+samuel.elbe@gmail.com
+*  pierrick.jacquette@gmail.com
+*
+* Remarques : Effectue en trinome
+*/
+
+#include "LibBT.h"
+
+int main(int argc, char const *argv[]) {
+	SIZE = 10;
+	return EXIT_SUCCESS;
+}
